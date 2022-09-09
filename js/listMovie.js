a3e02e9292c6c7f6f343c75aefdda3dd46ef87b5
19522515 <80948525+19522515@users.noreply.github.com>
2022-09-10 01:32:02 +0700

    Add files via upload

---
+++ js/listMovie.js
@@ -0,0 +1,36 @@
+function allMovies() {
+  const movieBlock = document.querySelectorAll(".movie__list");
+
+  function getAllMovies(callback) {
+    const movieApi = `https://api.themoviedb.org/3/movie/now_playing?api_key=e9e9d8da18ae29fc430845952232787c&language=en-US&page=1`;
+
+    fetch(movieApi)
+      .then((res) => res.json())
+      .then(callback);
+  }
+
+  function renderMovies(data) {
+    const { results } = data;
+    var htmls = results.map((movie, index) => {
+      return `
+        <li class="movie__item movie__item--${movie.id}">
+            <span class="movie__item-category">
+                ${movie.vote_average} <i class='bx bxs-star'></i>
+            </span>
+            <a class="movie__item-avt">
+              <img src="https://image.tmdb.org/t/p/original${
+                movie.poster_path
+              }" alt=""/>
+            </a>
+            <a class="movie__item-name">
+                <p>${movie.title} (${movie.release_date.slice(0, 4)})</p>
+            </a>
+        </li>`;
+    });
+    movieBlock.forEach(function (movie) {
+      movie.innerHTML = htmls;
+    });
+  }
+  getAllMovies(renderMovies);
+}
+export default allMovies;
